bfb37cb7acfa1a326a3cf88bfb90c57df049b810
KHLIFI BRAHIM <me.brahimkhlifi@gmail.com>
2025-02-25 11:55:29 +0000

    api

--- src/Api/axios.jsx
+++ src/Api/axios.jsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 const Instance = axios.create({
-    baseURL: 'https://backend-zeta-liart-77.vercel.app/api/',
+    baseURL: 'https://backend-zeta-liart-77.vercel.app/api',
     // baseURL: 'http://localhost:5000/api'
 })
 
